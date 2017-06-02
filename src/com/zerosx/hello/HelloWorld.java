4a7b640bb72b11e6c9a14ad936d164a718553ba6
rithikied <rithikied@hotmail.com>
2017-06-02 04:47:39 -0700

    write KUY by switch-case

--- src/com/zerosx/hello/HelloWorld.java
+++ src/com/zerosx/hello/HelloWorld.java
@@ -4,4 +4,16 @@ package com.zerosx.hello;
  * Created by root on 6/2/17.
  */
 public class HelloWorld {
+    public static void main(String[]args){
+           int i=1;
+            switch (i){
+                case 1:
+                    System.out.println("K");
+                case 2:
+                    System.out.println("U");
+                case 3:
+                    System.out.println("Y");
+            }
+        }
+
 }
